c3b57541870a1bb26bc7b26e6013da752e5dc7ca
Serge Adeninski <serge.adens@gmail.com>
2020-08-18 19:24:39 +0300

    initial

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,28 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+
+fun User.toUserView(): UserView {
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status =
+        if (lastVisit == null) "Еще ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit.humanizeDiff()}"
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        nickName = nickName,
+        avatar = avatar,
+        initials = initials,
+        status = status
+    )
+}
+
+private fun Utils.toInitials(firstName: String?, lastName: String?): String? {
+    TODO("Not yet implemented")
+}
+
+private fun Utils.transliteration(payload: String, divider: String = " "): String {
+    TODO("Not yet implemented")
+}
