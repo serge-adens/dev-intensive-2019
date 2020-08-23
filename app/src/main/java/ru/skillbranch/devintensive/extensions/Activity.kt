75bcf2865096b2c1080c1a16e9db133ee5358e23
Serge Adeninski <serge.adens@gmail.com>
2020-08-23 16:31:48 +0300

    hometask 3

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,18 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard(): Unit {
+    val imm: InputMethodManager = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    //Find the currently focused view, so we can grab the correct window token from it.
+    //Find the currently focused view, so we can grab the correct window token from it.
+    var view: View? = this.currentFocus
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    if (view == null) {
+        view = View(this)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
